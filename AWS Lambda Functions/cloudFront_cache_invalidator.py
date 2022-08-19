9d175d67a6ad3078328ab6185ea313333e00b0b2
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2022-08-19 15:17:13 +0530

    Update cloudFront_cache_invalidator.py

--- AWS Lambda Functions/cloudFront_cache_invalidator.py
+++ AWS Lambda Functions/cloudFront_cache_invalidator.py
@@ -4,6 +4,7 @@ Created on Tue May 17 20:31:39 2022
 
 @author: Rishi
 """
+    # This lambda function will detecet file changes and create invalidations for both react and html folder
 
 import json
 import boto3
@@ -11,32 +12,22 @@ import urllib.parse
 import time
 
 
-   
-
 cf=boto3.client('cloudfront')
-dist_id = 'E15RZDX35Q5QHZ'
+user_data_dist_id = 'cdn-id-of-html-files'
+build_dist_id = 'cdn-of-react'
 
-def lambda_handler(event, context):
-    
+
+def lambda_handler(event, context): 
     # TODO implement
     
-    #bucket = event['Records'][0]['s3']['bucket']['name']
+    response=""  
     
-    
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    path = "/"+key.split('/')[1]
-    print(path)
-    response=""
-    
-    
-    def invalidate():
-        
+    def invalidate_user_data(path):  
         
         try:
-            
-            
+                   
             response = cf.create_invalidation(
-            DistributionId= 'E15RZDX35Q5QHZ'   ,
+            DistributionId= user_data_dist_id  ,
             InvalidationBatch={
                 'Paths': {
                     'Quantity': 1,
@@ -54,14 +45,50 @@ def lambda_handler(event, context):
 
         except e:
             print("error",e)
+            
+            
     
-    
-    invalidate()
-   
+    def invalidate_build_file(path):
+        
+        
+        try:
+            
+            
+            response = cf.create_invalidation(
+            DistributionId=  build_dist_id  ,
+            InvalidationBatch={
+                'Paths': {
+                    'Quantity': 1,
+                    'Items': [
+                        path
+                    ]
+                },
+                'CallerReference': str(time.time()).replace(".", "")
+                 }
+              )
+        
+            #invalidation_id = response['Invalidation']['Id']
+            print("Done")
+      
 
+        except e:
+            print("error",e)
+            
+            
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    folder = key.split('/')[0]
+    print(folder)
+    
+    if (folder=='user-data'):
+        path = "/"+key.split('/')[1]
+        print(path)
+        invalidate_user_data(path)
+        
+    if (folder=='build'):
+        path = "/"+key.split('/')[2]
+        print(path)
+        invalidate_build_file(path)
         
     
     
-    
-
-    
+    print(key)
